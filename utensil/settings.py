c7beed83108fcb781f879dfa5f43b70420fe2aa8
yash kansal <yashkansal1611@gmail.com>
2025-11-06 16:29:29 +0530

    settings.py

--- utensil/settings.py
+++ utensil/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
-
+DEBUG = False
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
